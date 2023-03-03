8eda9a721fb73e1232b12dfbe3095ef3a573c06d
kienkcp <kentdaokcp@gmail.com>
2023-03-02 20:28:23 -0600

    Create Consumer_Complaints_Analysis.R

---
+++ Consumer_Complaints_Analysis.R
@@ -0,0 +1,11 @@
+library(dplyr)
+library(readxl)
+library(lubridate)
+library(sentimentr)
+library(foreign)
+library(wordcloud)
+
+setwd("D:/DATA 332/Github Project 1")
+
+df <- readRDS("Consumer_Complaints.rds") 
+
