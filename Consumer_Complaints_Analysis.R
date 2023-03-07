332668dfeecffae7bb0e03f4e4dbcd74e9ac91e9
kienkcp <kentdaokcp@gmail.com>
2023-03-07 00:51:41 -0600

    a

--- Consumer_Complaints_Analysis.R
+++ Consumer_Complaints_Analysis.R
@@ -15,6 +15,7 @@ library(textdata)
 library(ggthemes)
 library(radarchart)
 library(treemap)
+library(shiny)
 
 setwd("D:/DATA 332/Github Project 1")
 
@@ -56,4 +57,58 @@ wordcloud(df_2$sentiment, scale=c(5,0.5),
           min.freq = 1, max.words = Inf, 
           random.order = FALSE,
           rot.per = 0.35, colors = brewer.pal(8, "Dark2"))
+
+df_3 <- tidy_data %>%
+  inner_join(lexicon) %>%
+  group_by(word) %>%
+  count(sentiment, sort = TRUE)
+
+df_3%>%
+  ggplot(aes(n, word, fill = sentiment ))+
+  geom_col(show.legend = FALSE)+
+  facet_wrap(~sentiment, scales = "free_y") +
+  theme_light()+
+  labs(y = "sentimetal analysis", 
+       subtitle = "                                                                              NRC lexicon",
+       x=NULL, y=NULL)
+
+df_4 <-df_3 %>%
+  group_by(sentiment) %>%
+  summarise(n = n()) %>%
+  ungroup() %>%
+  mutate(sentiment = reorder(sentiment, n)) %>%
+  #Use `fill = -n` to make the larger bars darker
+  ggplot(aes(sentiment,n , fill = -n)) +
+  geom_col() +
+  guides(fill = "none") + #Turn off the legend
+  labs(x = NULL, y = "Word Count") +
+  #  scale_y_continuous(limits = c(0, 500000)) + #Hard code the axis limit
+  ggtitle("                                        Consumer Narrative NRC Sentiment") +
+  coord_flip()
+plot(df_4)
+
+
+
+ui <- pageWithSidebar(
+  
+  # App title ----
+  headerPanel("Consumer Complaint"),
+  
+  # Sidebar panel for inputs ----
+  sidebarPanel(
+    
+    # Input: Selector for variable to plot against mpg ----
+    selectInput("variable", "Variable:", 
+                c("Cylinders" = "cyl",
+                  "Transmission" = "am",
+                  "Gears" = "gear")),
+    
+    # Input: Checkbox for whether outliers should be included ----
+    checkboxInput("outliers", "Show outliers", TRUE)
+    
+  ),
+  
+  # Main panel for displaying outputs ----
+  mainPanel()
+)
 
